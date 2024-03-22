2d3eca07d3e7c725431e518e4dbd532522458e2f
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-03-22 09:54:04 +0900

    - 작업: 플레이어 캐릭터 생성 / .gitignore - 계획 : 스켈레탈 메시 설정하기

---
+++ FakeRunner/Source/FakeRunner/GameInfo.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "EngineMinimal.h"	// "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "GameInfo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FAKERUNNER_API UGameInfo : public UObject
+{
+	GENERATED_BODY()
+	
+};
