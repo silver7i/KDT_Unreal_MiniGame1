8f1cb7106e77738d5d2e6feb871fcf007c053bea
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-03-26 18:53:06 +0900

    -게임 모드 설정 ---------------------- >다음 일정 : 카메라 세팅 / 동작 확인

---
+++ FakeRunner/Source/FakeRunner/GameMode/MainGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../GameInfo.h"
+#include "GameFramework/GameModeBase.h"
+#include "MainGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FAKERUNNER_API AMainGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
